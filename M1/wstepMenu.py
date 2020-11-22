1f2cd3cafe06537de1cf5dbd51acb531c5278ce5
Kamil Tyrek <tyrekkamil@gmail.com>
2020-11-22 15:01:22 +0100

    update

--- M1/wstepMenu.py
+++ M1/wstepMenu.py
@@ -4,10 +4,10 @@ from mnozenieLiczb import multiply
 from dzielenieReszta import div
 from tobinary import toBinary, makeBinary
 from szybkiePotegowanie import exp
-
+from czyModPJestKwad import isModSqr
 
 def main():
-    print("1. Dodawanie\n2. Mnozenie\n3. Dzielenie z resztą\n4. Szybkie potęgowanie")
+    print("1. Algorytm dodawania pisemnego\n2. Algorytm mnożenia pisemnego\n3. Algorytm dzielenia z resztą\n4. Algorytm szybkiego potęgowania\n5. Algorytm sprawdzający czy reszta modulo p jest kwadratem")
     menuOption = input()
     switcher(menuOption)
 
@@ -39,19 +39,25 @@ def switcher(option):
         y = makeBinary(y)
         print(x, y, div(x, y))
     elif option == '4':
-        print("(b^k)%n | Podaj b w postaci dziesiętnej")
-        b = input()
-        print("(b^k)%n | Podaj k w postaci dziesiętnej")
-        k = input()
-        print("(b^k)%n | Podaj n w postaci dziesiętnej")
+        print("(b^k)%n | Podaj n w postaci binarnej")
         n = input()
+        print("(b^k)%n | Podaj b w postaci binarnej")
+        b = input()
+        print("(b^k)%n | Podaj k w postaci binarnej")
+        k = input()
         if int(b) < 2 or int(b) > int(n):
             print("b nie spełnia warunku: 2 <= b < n")
             return 0
-        b = toBinary(b)
-        k = toBinary(k)
-        n = toBinary(n)
-        print(b, k, exp(b, k, n))
+        print(n, b, k, exp(b, k, n))
+    elif option == '5':
+        print("Czy reszta modulo p jest kwadrtem? | Podaj p w postaci binarnej")
+        p = input()
+        print("Czy reszta modulo p jest kwadratem? | Podaj m w postaci binarnej")
+        m = input()
+        p = makeBinary(p)
+        m = makeBinary(m)
+        print(isModSqr(p, m))
+
     else:
         print("brak opcji, wybierz ponownie")
         switcher(input())
