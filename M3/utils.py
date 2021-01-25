75fd6995e235b709588984cd0dc978d01c841a08
Kamil Tyrek <tyrekkamil@gmail.com>
2021-01-25 17:36:18 +0100

    m3z2

--- M3/utils.py
+++ M3/utils.py
@@ -1,42 +1,47 @@
 from random import randint
 
+
 def generateElipcticCurve(p):
-    result = False # flaga, której końcowa wartość definuje, czy z danych wartości stworzymy krzywą eliptyczną
-    while result == False: # dopóki jest false...
-        A = randint(0, p - 1 )
-        B = randint(0, p - 1) # losowe A B
-        delta = (4 * pow(A, 3, p) + 27 * pow(B, 2, p)) % p # warunek 4A3 + 27B2 = 0 (mod p)
-        if delta != 0: # jeśli nie jest równe, para spełnia warunek - true
+    result = False  # flaga, której końcowa wartość definuje, czy z danych wartości stworzymy krzywą eliptyczną
+    while result == False:  # dopóki jest false...
+        A = randint(0, p - 1)
+        B = randint(0, p - 1)  # losowe A B
+        # warunek 4A3 + 27B2 = 0 (mod p)
+        delta = (4 * pow(A, 3, p) + 27 * pow(B, 2, p)) % p
+        if delta != 0:  # jeśli nie jest równe, para spełnia warunek - true
             result = True
-    return A, B, p 
+    return A, B, p
 
 
 def znajdzLosowyPkt(A, B, p):
     x = randint(0, p)
-    y = randint(0, p) # losowanie x i y z przedziału (0, p) - bez p 
+    y = randint(0, p)  # losowanie x i y z przedziału (0, p) - bez p
 
-    while not (czyNalezy(A, B, p, x, y)): # warunek punktu krzywej
+    while not (czyNalezy(A, B, p, x, y)):  # warunek punktu krzywej
         x = randint(0, p)
         y = randint(0, p)
     return x, y
 
+
 def czyNalezy(A, B, p, x, y):
-    result = False # kontrolka - domyslnie false, przy spełnionym warunki TRUE
-    if ((y**2) % p) == ((pow(x, 3, p) + A*x + B) % p): # y2 = x3 + Ax + B oraz p jako modulo
+    result = False  # kontrolka - domyslnie false, przy spełnionym warunki TRUE
+    if ((y**2) % p) == ((pow(x, 3, p) + A*x + B) % p):  # y2 = x3 + Ax + B oraz p jako modulo
         return True
     return result
 
-def multiple_point(x, y, n, a, b, p):
+
+def multiple_point(x, y, n, A, B, p):
     xq = x
     yq = y
     xr, yr = None, None
     while n > 0:
         if n % 2 == 1:
-            xr, yr = dodajPktNaKrzywej(xr, yr, xq, yq, a, b, p)
+            xr, yr = dodajPktNaKrzywej(A, B, p, xr, yr, xq, yq)
             n -= 1
-        xq, yq = dodajPktNaKrzywej(xq, yq, xq, yq, a, b, p)
+        xq, yq = dodajPktNaKrzywej(A, B, p, xq, yq, xq, yq)
         n = n // 2
     return xr, yr
+
 
 def dodajPktNaKrzywej(A, B, p, x1, y1, x2, y2):
     # Przypadek P = -Q
@@ -48,28 +53,30 @@ def dodajPktNaKrzywej(A, B, p, x1, y1, x2, y2):
         return x1, y2
     # Przypadek P != Q
     elif (x1) != (x2):
-        lam = ((y2 - y1) * (modinv(p, (x2-x1) % p))[1]) % p
-        x3 = (pow(lam, 2, p) - x1 - x2) % p
+        lam = ((y2 - y1) * (pow((x2-x1) % p, -1, p))) % p
+        x3 = (pow(lam % p, 2, p) - x1 - x2) % p
         y3 = (lam * (x1 - x3) - y1) % p
         return x3, y3
     # Przypadek P = Q
     elif (x1, y1) == (x2, y2):
-        lam = (((3 * pow(x1, 2)) + A) * (pow((2 * y1), -1, p))) % p
-        x3 = (pow(lam, 2) - 2 * x1) % p
+        lam = (((3 * pow(x1, 2, p)) + A) * (pow((2 * y1) % p, -1, p))) % p
+        x3 = (pow(lam, 2, p) - 2 * x1) % p
         y3 = (lam * (x1 - x3) - y1) % p
         return x3, y3
 
-def exp(b, k, n): #n b k -> b k n
-    r = [1] 
+
+def exp(b, k, n):  # n b k -> b k n
+    r = [1]
     x = div(b, n)[1]
-    
+
     for i in range(len(k) - 1, -1, -1):
         if k[i] == 1:
             r = multiply(r, x)
             r = div(r, n)[1]
-        x  = multiply(x, x)
-        x  = div(x, n)[1]
+        x = multiply(x, x)
+        x = div(x, n)[1]
     return r
+
 
 def toBinary(a):
     if isinstance(a, str):
@@ -81,11 +88,13 @@ def toBinary(a):
 def makeBinary(x):
     return [int(d) for d in str(x)]
 
+
 def binaryToDec(x):
     result = 0
     for i in range(len(x)-1, -1, -1):
         result = result + x[i] * pow(2, i)
     return result
+
 
 def multiply(a, b):
     B = 2
@@ -101,6 +110,7 @@ def multiply(a, b):
             c, d[i+j] = Q(tmp, B)
         d[i+len(b)] = c
     return optimize(d)
+
 
 def div(a, b):
     if len(b) > len(a):
@@ -138,25 +148,26 @@ def div(a, b):
     return(q, r)
 
 
-
 def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
-    g, x, y = egcd(b%a, a)
-    
+    g, x, y = egcd(b % a, a)
+
     tmp = x
     x = y - (b//a) * x
     y = tmp
     return (g, x, y)
- 
+
+
 def modinv(a, m):
     g, x, y = egcd(a, m)
     if g == 1:
         if m == 1:
             return [1]
-        return g, x%m
+        return g, x % m
     else:
         return g, []
+
 
 def Q(x, y):
     return x//y, (x % y)
@@ -168,13 +179,14 @@ def swap(a, b):
     b = tmp
     return a, b
 
+
 def optimize(x):
     index = 0
     x = x[::-1]
     for i in range(0, len(x) - 1):
         if(x[i] == 0):
-            index = index + 1 
-        else: 
+            index = index + 1
+        else:
             break
     x = x[index:]
     return x
--- M3/zad2.py
+++ M3/zad2.py
@@ -3,27 +3,31 @@ from utils import generateElipcticCurve, znajdzLosowyPkt, dodajPktNaKrzywej, mul
 from math import sqrt
 
 
-def losujP():
-    p = randint(0, 10000000)
+def losujBit(bits):
+    s, i = 0, 0
+    m = 1
+    for i in range(0, bits):
+        s = s + randint(0, 1) * m 
+        m = m * 2
+        i = i + 1
+    return s
+
+def losujP(bits):
+    p = losujBit(bits)
     if p % 4 != 3:
-        return losujP()
+        return losujP(bits)
     else:
         return p
 
 
-def GenerujKlucze(): #P K BITÓW!!!!!TODO
-    A, B, p = generateElipcticCurve(losujP())
+def GenerujKlucze():  # TODO czasami nie ma odwrotnosci
+    bits = 10
+    A, B, p = generateElipcticCurve(losujP(bits))  # generowanie krzywej
     xp, yp = znajdzLosowyPkt(A, B, p)
-    tmpMaxX = int(p + 1 - 2 * sqrt(p))
+    tmpMaxX = int(p + 1 - 2 * sqrt(p))  # maksymalna wartość X
     x = randint(1, tmpMaxX)
     xq, yq = multiple_point(xp, yp, x, A, B, p)
-    print("K_A = [\n\tE = [\n\t\t A =", A, "\n\t\t B =", B, "\n\t\t]\n\tp =", p, "\n\tP = (\n\t\tx =", xp,
-          "\n\t\ty =", yp, "\n\t\t)\n\tQ = (\n\t\tx =", xq,
-          "\n\t\ty =", yq, "\n\t\t)\n\t]")
-    print("k_A = [\n\tE = [\n\t\t A =", A, "\n\t\t B =", B, "\n\t\t]\n\tp =", p, "\n\tP = (\n\t\tx =", xp,
-          "\n\t\ty =", yp, "\n\t\t)\n\tQ = (\n\t\tx =", xq,
-          "\n\t\ty =", yq, "\n\t\t)\n\tx =", x, "\n\t]")
-    return 0
+    return A, B, xp, yp, xq, yq, x
 
 
 print(GenerujKlucze())
