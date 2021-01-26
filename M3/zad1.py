3f2137cf6a329d6dac4bb28d0e7def1845a4e72f
Kamil Tyrek <tyrekkamil@gmail.com>
2021-01-26 21:22:06 +0100

    zad 1 i 4 gotowe

--- M3/zad1.py
+++ M3/zad1.py
@@ -1,15 +1,27 @@
 from utils import dodajPktNaKrzywej, generujPrzeciwny
 
-def wielokrotnoscPkt(A, B, p, x, y, n): #TODO ujemne
+def wielokrotnoscPkt(A, B, p, x, y, n):
 
     xq = x
     yq = y
     xr, yr = None, None
-    
+
+    sign = 1
+    if n < 0: # sprawdzenie znaku
+        sign = -1
+        n = n * -1 # jezeli jest ujemne to sign = -1, z n "usuwamy minusa"
+ 
     while n > 0:
         if n % 2 == 1:
             xr, yr = dodajPktNaKrzywej(A, B, p, xr, yr, xq, yq)
             n = n - 1 # pomnożenie przez znak - aby wiedział, w którą stronę iść (dod/uj)
         xq, yq = dodajPktNaKrzywej(A, B, p, xq, yq, xq, yq)
         n = n // 2
+    if sign == -1:
+        xr, yr = generujPrzeciwny(p, xr, yr) # Traktujemy cały czas jak dodanie, dopiero na końcu mnożymy przez minusa, który jest przy "n" - czyli generujemy element przeciwny
     return xr, yr
+
+def Zad1(A, B, p, x, y, n):
+    return A, B, p, x, y, wielokrotnoscPkt(A, B, p, x, y, n), n
+
+#print(Zad1(138,68,157,25,60,-130))
--- M3/zad3.py
+++ M3/zad3.py
@@ -1,12 +1,14 @@
 from random import randint
 from utils import isModSqr, znajdzB
 
-kappa_default=100
+kappa_default = 100
+
 
 def decode(xpm, kappa):
     return (xpm) // kappa
 
-def encode(A, B, p, M, kappa=kappa_default): #TODO y, x czasami blisskie
+
+def encode(A, B, p, M, kappa=kappa_default):  # TODO y, x czasami blisskie
     N = randint(1, int(0.05 * p)) + M
 
     if N * kappa < p:
@@ -14,15 +16,19 @@ def encode(A, B, p, M, kappa=kappa_default): #TODO y, x czasami blisskie
             x = (M * kappa + j) % p
             f = (pow(x, 3, p) + A * x + B) % p
             if isModSqr(f, p):
-                y1, y2 = znajdzB(f, p)
+                y1, y2 = znajdzB(f, p) ##
                 if y1 > 0:
                     return x, y1, decode(x, kappa) == M
                 else:
                     return x, y2, decode(x, kappa) == M
     return None, None
 
-result = encode(550887326258000 ,761654032839972 ,1008614860794511 ,8092922464793 )
-while result[0] is None or result[1] is None:
-    result = encode(550887326258000 ,761654032839972 ,1008614860794511 ,8092922464793 )
 
-print(result)
+def Zad3(A, B, p, M, kappa=kappa_default):
+    result = encode(A, B, p, M)
+    while result[0] is None or result[1] is None:
+        result = encode(A, B, p, M)
+    return A, B, p, kappa, M, result
+
+
+print(Zad3(550887326258000, 761654032839972, 1008614860794511, 8092922464793))
