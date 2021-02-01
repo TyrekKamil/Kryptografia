a805592ce819a1fc86fa6652c9a1eb63afff383e
Kamil Tyrek <tyrekkamil@gmail.com>
2021-02-01 20:21:48 +0100

    zad2 complete

--- M4/Zad2.py
+++ M4/Zad2.py
@@ -12,39 +12,42 @@ def xTime(ab, c):
         ab.insert(0, 0)
     result = []
     if int(ab[0]) == 1:
-        #    result.append(str(int(ab[7]) * c[0]))
-        for i in range(7, 1, -1):
-            tmp = ((c[i]) + int(ab[i-1])) % 2 # 0
+        ab = ab[::-1]
+        c = c[::-1]
+        for i in range(7, 0, -1):
+            tmp = (c[i] + int(ab[i-1])) % 2
             result.append(str(tmp))
         result.append(c[0])
     elif int(ab[0]) == 0:
         result = ab[1:]
         while len(result) < 8:
             result.append(str(0))
-    #result = reversed(result)
-    result = [str(x) for x in result]
-    return BinToHex(result)
-# 97 1001 0111
-# 45 0100 0101
 
+    result = [str(x) for x in result]
+    return BinToHex(result).upper()
+# 92 1001 0010
+# 11 0001 0001
+# 3f 0011 1111
 
 def Zad2():
     ab = RandomHex()
     f = losujF()
-    ab = "93"
-    f = [1, 1, 0, 1, 1, 0, 0, 0, 1]
+    ab = "92"
+    f = [1, 0, 0, 0, 1, 1, 0, 1, 1]
     print(ab, xTime(ab, f), f)
 
 def przykladoweWejscia():
-    file1 = open('./Wejscia/Zad2.txt', 'r')
+    file1 = open('Wejscia/zad2.txt', 'r')
     Lines = file1.readlines() 
-    a, b, c = [], [], []
+
     for line in Lines:
         arr = line.split(";")
+
         arr[2] = arr[2].strip()
-        a.append(arr[0])
-        b.append(arr[1])
-        c.append(arr[2])
+        arr[2] = arr[2][1:-1]
+        arr[2] = arr[2].split(",")
+        arr[2] = [int(x) for x in arr[2]]
+
         print(arr[0], xTime(arr[0], arr[2]), arr[1], " Czy wynik się zgadza?", arr[1] == xTime(arr[0], arr[2]))
 
 przykladoweWejscia()
